6105e1315131462bc8b547864a3f96bc9248b942
Francis Le Roy <francis.leroy@protonmail.ch>
2021-04-30 02:00:22 +0200

    Fixed compilation errors

--- basiliq_db_test_utils/src/lib.rs
+++ basiliq_db_test_utils/src/lib.rs
@@ -1,7 +1,7 @@
 mod db;
 mod init_values;
 
-pub use db::{connect_to_management_pool, deinit_db, init_db, BASILIQ_DATABASE_URL};
+pub use db::{connect_to_management_pool, deinit_db, init_db, DATABASE_URL};
 pub use init_values::{
     ARTICLES_IDS, COMMENTS_IDS, FAVORITE_COLOR_IDS, PEOPLES_IDS, PEOPLE_ARTICLE_ID,
 };
